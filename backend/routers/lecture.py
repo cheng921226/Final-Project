7530500abb1bb2551e6c691d4f9316d0ec9cb75a
TSY0704 <yuyuyu910622@gmail.com>
2026-05-14 00:33:14 +0800

    新增課程查詢功能與摘要、知識點 API

--- backend/routers/lecture.py
+++ backend/routers/lecture.py
@@ -1,16 +1,33 @@
 from database.supabase import supabase
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 router = APIRouter()
 
-# 取得所有課程
+# 取得所有課程 + 輸入keyword可搜尋
 @router.get("/lecture")
-def get_lectures():
-    res = supabase.table("lectures").select("*").execute()
+def get_lectures(keyword : str = None):
+    query = supabase.table("lectures").select("*")
+
+    if keyword:
+        query = query.ilike("title", f"%{keyword}%")
+
+    res = query.execute()
     return res.data
 
 # 取得特定課程
 @router.get("/lecture/{lecture_id}")
 def get_seleted_lectures(lecture_id : int):
     res = supabase.table("lectures").select("*").eq("id",lecture_id).execute()
+    return res.data
+
+# 取得該課程摘要
+@router.get("/lecture/{lecture_id}/summary")
+def get_lecture_summary(lecture_id : int):
+    res = supabase.table("summaries").select("*").eq("lecture_id",lecture_id).single().execute()
+    return res.data
+
+# 取得該課程知識點
+@router.get("/lecture/{lecture_id}/knowledge_points")
+def get_lecture_knowledge_points(lecture_id : int):
+    res = supabase.table("knowledge_points").select("*").eq("lecture_id",lecture_id).execute()
     return res.data
