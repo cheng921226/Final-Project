6b7e4bd5f336e2e0a03c2fa2b990a76627c3bdeb
TSY0704 <yuyuyu910622@gmail.com>
2026-05-24 17:28:17 +0800

    修正email驗證格式

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,45 +1,46 @@
-from database.supabase import supabase
+from database.supabase import supabase_admin
 from fastapi import APIRouter,HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 router = APIRouter()
 
 class RegisterRequest(BaseModel):
     name:str
-    email:str
+    email:EmailStr
     password:str
 
 @router.post("/register")
 def register(data : RegisterRequest):
     try:
-        res = supabase.auth.sign_up({
+        res = supabase_admin.auth.admin.create_user({
             "email":data.email,
-            "password":data.password
+            "password":data.password,
+            "email_confirm":True
         })
-
+        
         user=res.user
         
         if user is None:
-            raise HTTPException(400, "Register failed")
+            raise HTTPException(400, detail="Create user failed")
         
-        supabase.table("users").insert({
-            "id":user.id,
+        supabase_admin.table("users").insert({
             "name":data.name,
-            "email":data.email
+            "email":data.email,
+            "role":"student",
         }).execute()
 
         return {"message": "register success"}
     except Exception as e:
-        raise HTTPException(400,str(e))
+        raise HTTPException(400, detail=str(e))
 
 class LoginRequest(BaseModel):
-    email:str
+    email:EmailStr
     password:str
 
 @router.post("/login")
 def login(data : LoginRequest):
     try:
-        res = supabase.auth.sign_in_with_password({
+        res = supabase_admin.auth.sign_in_with_password({
             "email": data.email,
             "password": data.password
         })
@@ -54,7 +55,7 @@ def login(data : LoginRequest):
         }
 
     except Exception as e:
-        raise HTTPException(401, str(e))
+        raise HTTPException(401, detail=str(e))
 
 @router.post("/logout")
 def logout():
