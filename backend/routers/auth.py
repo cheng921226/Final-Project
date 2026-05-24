1a4dcacb34d83f007ec9e1a9f82da95a13d72b57
TSY0704 <yuyuyu910622@gmail.com>
2026-05-24 16:49:11 +0800

    註冊/登入/登出api

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,16 +1,61 @@
 from database.supabase import supabase
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
+from pydantic import BaseModel
 
 router = APIRouter()
 
+class RegisterRequest(BaseModel):
+    name:str
+    email:str
+    password:str
+
 @router.post("/register")
-def register():
-    return []
+def register(data : RegisterRequest):
+    try:
+        res = supabase.auth.sign_up({
+            "email":data.email,
+            "password":data.password
+        })
+
+        user=res.user
+        
+        if user is None:
+            raise HTTPException(400, "Register failed")
+        
+        supabase.table("users").insert({
+            "id":user.id,
+            "name":data.name,
+            "email":data.email
+        }).execute()
+
+        return {"message": "register success"}
+    except Exception as e:
+        raise HTTPException(400,str(e))
+
+class LoginRequest(BaseModel):
+    email:str
+    password:str
 
 @router.post("/login")
-def login():
-    return []
+def login(data : LoginRequest):
+    try:
+        res = supabase.auth.sign_in_with_password({
+            "email": data.email,
+            "password": data.password
+        })
+
+        return {
+            "access_token": res.session.access_token,
+            "refresh_token": res.session.refresh_token,
+            "user": {
+                "id": res.user.id,
+                "email": res.user.email
+            }
+        }
+
+    except Exception as e:
+        raise HTTPException(401, str(e))
 
 @router.post("/logout")
 def logout():
-    return []
+    return {"message": "logout success"}
