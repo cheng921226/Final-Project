99023408aec8ba2b335369a5a7f04b387054dbe1
liujuncheng <cheng921226@gmail.com>
2026-05-26 19:27:55 +0800

    上傳頁面以及路由

--- student-platform/src/App.js
+++ student-platform/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import CourseDetail from './CourseDetail';
+import LectureDetail from './LectureDetail';
+import UploadLecture from './UploadLecture';
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course/:id" element={<CourseDetail />} />
+        <Route path="/course/:id/lecture/:lectureId" element={<LectureDetail />} />
+        <Route path="/upload-lecture" element={<UploadLecture />} />
       </Routes>
     </Router>
   );
